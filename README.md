b94e21825a760f6fc5d99c20262f79b625d7b62e
julien deantoni <julien.deantoni@inria.fr>
2021-02-01 10:10:09 +0100

    initial push

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # polytechWebotsController
-This is a repo with a java based API ton control a cyberbotics robots (webots) based on the "create" robot (https://cyberbotics.com/doc/guide/create) with a pen and a gripper
+
+This is a repo used for teach (meta)language semantics with a java based API to control a cyberbotics robots (webots: ) based on the "create" robot (https://cyberbotics.com/doc/guide/create) augmented with a pen and a gripper
+
+It is supposed to be used with the GEMOC studio (gemoc.org/studio.html).
+
+In order to put the webots library in the classpath, one should do
+`export LD_LIBRARY_PATH=/snap/webots/current/usr/share/webots/lib/controller/` in the launching terminal with the appropriate path before to launch gemoc.
---
+++ fr.univcotedazur.kairos.webots.polycreate.controler/src/fr/univcotedazur/kairos/webots/polycreate/controler/PolyCreateControler.java
@@ -0,0 +1,273 @@
+package fr.univcotedazur.kairos.webots.polycreate.controler;
+
+import java.util.Random;
+
+import com.cyberbotics.webots.controller.Camera;
+import com.cyberbotics.webots.controller.CameraRecognitionObject;
+import com.cyberbotics.webots.controller.DistanceSensor;
+import com.cyberbotics.webots.controller.LED;
+import com.cyberbotics.webots.controller.Motor;
+import com.cyberbotics.webots.controller.Pen;
+import com.cyberbotics.webots.controller.PositionSensor;
+import com.cyberbotics.webots.controller.Receiver;
+import com.cyberbotics.webots.controller.Robot;
+import com.cyberbotics.webots.controller.TouchSensor;
+
+public class PolyCreateControler extends Robot {
+
+	static int MAX_SPEED = 16;
+	static int NULL_SPEED = 0;
+	static int HALF_SPEED = 8;
+	static int MIN_SPEED = -16;
+
+	static double WHEEL_RADIUS = 0.031;
+	static double AXLE_LENGTH = 0.271756;
+	static double ENCODER_RESOLUTION = 507.9188;
+
+	public Pen pen = null;
+
+	public Motor[] gripMotors = new Motor[2];
+	public DistanceSensor gripperSensor = null;
+
+	public Motor leftMotor = null;
+	public Motor rightMotor = null;
+
+	public PositionSensor leftSensor = null;
+	public PositionSensor rightSensor = null;
+
+	public LED ledOn = null;
+	public LED ledPlay = null;
+	public LED ledStep = null;
+
+	public TouchSensor leftBumper = null;
+	public TouchSensor rightBumper = null;
+
+	public DistanceSensor leftCliffSensor = null;
+	public DistanceSensor rightCliffSensor = null;
+	public DistanceSensor frontLeftCliffSensor = null;
+	public DistanceSensor frontRightCliffSensor = null;
+
+	public Camera frontCamera = null;
+	public Camera backCamera = null;
+
+	public Receiver receiver = null;
+
+	public 	int timestep = Integer.MAX_VALUE;
+	public 	Random random = new Random();
+
+
+
+	public PolyCreateControler() {
+		timestep = (int) Math.round(this.getBasicTimeStep());
+
+		pen = createPen("pen");
+
+		gripMotors[0] = createMotor("7");
+		gripMotors[1] = createMotor("7 left");
+		gripperSensor = createDistanceSensor("gripper middle distance sensor");
+
+		leftMotor = createMotor("left wheel motor");
+		rightMotor = createMotor("right wheel motor");
+		leftMotor.setPosition(Double.POSITIVE_INFINITY);
+		rightMotor.setPosition(Double.POSITIVE_INFINITY);
+		leftMotor.setVelocity(0.0);
+		rightMotor.setVelocity(0.0);
+
+		leftSensor = createPositionSensor("left wheel sensor");
+		rightSensor = createPositionSensor("right wheel sensor");
+		leftSensor.enable(timestep);
+		rightSensor.enable(timestep);
+
+		ledOn = createLED("led_on");
+		ledPlay = createLED("led_play");
+		ledStep = createLED("led_step");
+
+		leftBumper = createTouchSensor("bumper_left");
+		rightBumper = createTouchSensor("bumper_right");
+		leftBumper.enable(timestep);
+		rightBumper.enable(timestep);
+
+		leftCliffSensor = createDistanceSensor("cliff_left");
+		rightCliffSensor = createDistanceSensor("cliff_right");
+		frontLeftCliffSensor = createDistanceSensor("cliff_front_left");
+		frontRightCliffSensor = createDistanceSensor("cliff_front_right");
+		leftCliffSensor.enable(timestep);
+		rightCliffSensor.enable(timestep);
+		frontLeftCliffSensor.enable(timestep);
+		frontRightCliffSensor.enable(timestep);
+
+		frontCamera = createCamera("frontCamera");
+		frontCamera.enable(timestep);
+		frontCamera.recognitionEnable(timestep);
+
+		backCamera = createCamera("backCamera");
+		backCamera.enable(timestep);
+		backCamera.recognitionEnable(timestep);
+
+		receiver = createReceiver("receiver");
+		receiver.enable(timestep);
+
+		PolyCreateControler ctrl = this;
+		Runtime.getRuntime().addShutdownHook(new Thread()
+		{
+			@Override
+			public void run()
+			{
+				System.out.println("Shutdown hook ran!");
+				ctrl.delete();
+			}
+		});
+	}
+
+
+	void openGripper() {
+		gripMotors[0].setPosition(0.5);
+		gripMotors[1].setPosition(0.5);
+	}
+
+	void closeGripper() {
+		gripMotors[0].setPosition(-0.2);
+		gripMotors[1].setPosition(-0.2);
+	}
+
+	boolean isThereCollisionAtLeft() {
+		return (leftBumper.getValue() != 0.0);
+	}
+
+	boolean isThereCollisionAtRight() {
+		return (rightBumper.getValue() != 0.0);
+	}
+
+	void flushIRReceiver() {
+		while (receiver.getQueueLength() > 0)
+			receiver.nextPacket();
+	}
+
+	boolean isThereVirtualwall() {
+		return (receiver.getQueueLength() > 0);
+	}
+
+	boolean isThereCliffAtLeft() {
+		return (leftCliffSensor.getValue() < 100.0 || frontLeftCliffSensor.getValue() < 100.0);
+	}
+
+	boolean isThereCliffAtRight() {
+		return (rightCliffSensor.getValue() < 100.0 || frontRightCliffSensor.getValue() < 100.0);
+	}
+
+	boolean isThereCliffAtFront() {
+		return (frontLeftCliffSensor.getValue() < 100.0 || frontRightCliffSensor.getValue() < 100.0);
+	}
+
+	void goForward() {
+		leftMotor.setVelocity(MAX_SPEED);
+		rightMotor.setVelocity(MAX_SPEED);
+	}
+
+	void goBackward() {
+		leftMotor.setVelocity(-HALF_SPEED);
+		rightMotor.setVelocity(-HALF_SPEED);
+	}
+
+	void stop() {
+		leftMotor.setVelocity(NULL_SPEED);
+		rightMotor.setVelocity(NULL_SPEED);
+	}
+
+	void passiveWait(double sec) {
+		double start_time = getTime();
+		do {
+			step(timestep);
+		} while (start_time + sec > getTime());
+	}
+
+	double randdouble() {
+		return  random.nextDouble();
+	}
+
+	void turn(double angle) {
+		stop();
+		double l_offset = leftSensor.getValue();
+		double r_offset = rightSensor.getValue();
+		step(timestep);
+		double neg = (angle < 0.0) ? -1.0 : 1.0;
+		leftMotor.setVelocity(neg * HALF_SPEED);
+		rightMotor.setVelocity(-neg * HALF_SPEED);
+		double orientation;
+		do {
+			double l = leftSensor.getValue() - l_offset;
+			double r = rightSensor.getValue() - r_offset;
+			double dl = l * WHEEL_RADIUS;                 // distance covered by left wheel in meter
+			double dr = r * WHEEL_RADIUS;                 // distance covered by right wheel in meter
+			orientation = neg * (dl - dr) / AXLE_LENGTH;  // delta orientation in radian
+			step(timestep);
+		} while (orientation < neg * angle);
+		stop();
+		step(timestep);
+	}
+
+
+
+
+	public static void main(String[] args) {
+		PolyCreateControler controler = new PolyCreateControler();
+
+		try {
+			controler.openGripper();
+			controler.pen.write(true);
+			controler.ledOn.set(1);
+			controler.passiveWait(0.5);
+
+			while (true) {
+				/**
+				 * The position and orientation are expressed relatively to the camera (the relative position is the one of the center of the object which can differ from its origin) and the units are meter and radian.
+				 * https://www.cyberbotics.com/doc/reference/camera?tab-language=python#wb_camera_has_recognition
+				 */
+				CameraRecognitionObject[] backObjs = controler.backCamera.getCameraRecognitionObjects();
+				if (backObjs.length > 0) {
+					CameraRecognitionObject obj = backObjs[0];
+					double[] backObjPos = obj.getPosition();
+					System.out.println("I saw an object on back Camera at : "+backObjPos[0]+","+backObjPos[1]);
+				}
+				CameraRecognitionObject[] frontObjs = controler.backCamera.getCameraRecognitionObjects();
+				if (frontObjs.length > 0) {
+					CameraRecognitionObject obj = frontObjs[0];
+					double[] frontObjPos = obj.getPosition();
+					System.out.println("I saw an object on front Camera at : "+frontObjPos[0]+","+frontObjPos[1]);
+				}
+				if (controler.isThereVirtualwall()) {
+					System.out.println("Virtual wall detected\n");
+					controler.turn(Math.PI);
+				} else if (controler.isThereCollisionAtLeft() || controler.isThereCliffAtRight()) {
+					System.out.println("Left obstacle detected\n");
+					controler.goBackward();
+					controler.passiveWait(0.5);
+					controler.turn(Math.PI * controler.randdouble());
+				} else if (controler.isThereCollisionAtRight()|| controler.isThereCliffAtRight() || controler.isThereCliffAtFront()) {
+					System.out.println("Right obstacle detected\n");
+					controler.goBackward();
+					controler.passiveWait(0.5);
+					controler.turn(-Math.PI * controler.randdouble());
+				} else {
+					controler.goForward();
+				}
+				controler.flushIRReceiver();
+				controler.step(controler.timestep);
+			}
+
+		}catch (Exception e) {
+			controler.delete();
+		}
+
+
+
+	}
+
+
+	@Override
+	protected void finalize() {
+		this.delete();
+		super.finalize();
+	}
+
+}
