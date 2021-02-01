47ee7483029fe2efbfec6c6f1f45cca4ad57fa1e
julien deantoni <julien.deantoni@inria.fr>
2021-02-01 11:06:15 +0100

    add licensing

--- fr.univcotedazur.kairos.webots.polycreate.controler/src/fr/univcotedazur/kairos/webots/polycreate/controler/PolyCreateControler.java
+++ fr.univcotedazur.kairos.webots.polycreate.controler/src/fr/univcotedazur/kairos/webots/polycreate/controler/PolyCreateControler.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2017 I3S and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ *     UCA - I3S Laboratory - julien.deantoni@univ-cotedazur.fr -> initial API
+ *******************************************************************************/
+
 package fr.univcotedazur.kairos.webots.polycreate.controler;
 
 import java.util.Random;
